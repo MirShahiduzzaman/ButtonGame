a079cd7a937bf3308c7dbfa4dfc3ae1db2ea1ab5
AfaqAnwar <afaqanwar.se@outlook.com>
2019-01-06 16:04:51 -0500

    Implemented TimerTask generation method in order to animate Buttons.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -19,29 +19,43 @@ public class Controller
     @FXML
     Button green;
 
-    Timer timer = new Timer();
-    TimerTask task = new TimerTask() {
-        @Override
-        public void run() {
-            blue.setOpacity(1);
-        }
-    };
+    Timer buttonTimer = new Timer();
 
     public void bluePress()
     {
         blue.setOpacity(0.5);
-        timer.scheduleAtFixedRate(task,500,500);
+        buttonTimer.scheduleAtFixedRate(createNewTask(blue),500,500);
     }
     public void redPress()
     {
         red.setOpacity(0.5);
+        buttonTimer.scheduleAtFixedRate(createNewTask(red),500,500);
     }
     public void yellowPress()
     {
         yellow.setOpacity(0.5);
+        buttonTimer.scheduleAtFixedRate(createNewTask(yellow),500,500);
     }
     public void greenPress()
     {
         green.setOpacity(0.5);
+        buttonTimer.scheduleAtFixedRate(createNewTask(green),500,500);
+    }
+
+    /** Afaq Anwar
+     * Generates a TimerTask to set the opacity to 1 for a specified Button.
+     *      TimerTasks cannot be reused, thus the creation of a new TimerTask is needed.
+     *      A more elegant solution would be to use a SequentialTransition with a FillTransition.
+     * @param button Any Button within the current Scene.
+     * @return TimerTask that sets the button opacity to 1.
+     */
+    private TimerTask createNewTask(Button button) {
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                button.setOpacity(1);
+            }
+        };
+        return task;
     }
 }
