d826aaabdee7904981d596cac263617b47af677c
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2019-01-06 14:06:05 -0500

    Made pressButton Method
    Signed-off-by: Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,9 +7,14 @@ import javafx.scene.control.Button;
 
 public class Controller
 {
-    @FXML
-    private Button blue;
+    /*@FXML
+    Button blue;
     public void pressButton() {
         blue.setOpacity(0.5);
+    }*/
+
+    public void pressButton(ActionEvent event)
+    {
+        System.out.println("Hello World");
     }
 }
